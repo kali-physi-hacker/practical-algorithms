a34bcdd80c801152309ce444380ed85784983c45
Desmond Brown <browndesmond30@yahoo.com>
2024-07-23 17:34:32 +0000

    Implemented solution for a code challenge (problem) using dyck paths and acceptable sequence algorithm

--- catalan_numbers/dyck_paths.py
+++ catalan_numbers/dyck_paths.py
@@ -1,22 +1,23 @@
 """
-Catalan numbers
----------------
-Catalan numbers are a sequence of positive integers that are found in many counting problems in combinatorics.
-They count certain  types of:
-- lattice paths
-- permutations
-- binary trees
-- other combinatorial objects
-They satisfy a fundamental recurrence relation, and have a closed-form formula in terms of binomial coefficients.
------------------------------------------------------------------------------------------------------------------
-Definition
----------
-C0, C1, C2, ... Cn, ... are given by the forumla:
-Cn = (1/(n+1))(2n/n)
-=> C0 = 1
-=> C1 = 1
-=> C2 = 2
-=> C3 = 5
-=> C4 = 14
-=> C5 = 42
+Question:
+Have the function bracket_combinations(num) read
+num which will be an integer greater than or equal to
+zero, and return the number of valid combinations that
+can be formed with num pairs of parentheses. For
+example, if the input is 3, then the possible combinations
+of 3 pairs of parenthesis, namely: ()()(), are ()()(), ()(()),
+(())(), ((())), and (()()). There are 5 total combinations
+when the input is 3, so your program should return 5.
 """
+import math
+
+
+def bracket_combinations(num: int) -> int:
+    if num == 0:
+        return 1
+    return math.comb(2*num, num)//(num + 1)
+    # keep this function call here
+
+
+if __name__ == '__main__':
+    print(bracket_combinations(int(input())))
