1a54940f591c19631d4a406131c05a8184720853
Desmond Brown <browndesmond30@yahoo.com>
2024-07-23 16:40:55 +0000

    Added definition for catalan numbers

---
+++ catalan_numbers/dyck_paths.py
@@ -0,0 +1,22 @@
+"""
+Catalan numbers
+---------------
+Catalan numbers are a sequence of positive integers that are found in many counting problems in combinatorics.
+They count certain  types of:
+- lattice paths
+- permutations
+- binary trees
+- other combinatorial objects
+They satisfy a fundamental recurrence relation, and have a closed-form formula in terms of binomial coefficients.
+-----------------------------------------------------------------------------------------------------------------
+Definition
+---------
+C0, C1, C2, ... Cn, ... are given by the forumla:
+Cn = (1/(n+1))(2n/n)
+=> C0 = 1
+=> C1 = 1
+=> C2 = 2
+=> C3 = 5
+=> C4 = 14
+=> C5 = 42
+"""
---
+++ catalan_numbers/theory.md
@@ -0,0 +1,13 @@
+# Catalan Numbers
+Catalan numbers are a sequence of positive integers that are found in many counting problems in combinatorics.   
+They count certain  types of:   
+- lattice paths
+- permutations
+- binary trees
+- other combinatorial objects
+
+They satisfy a fundamental recurrence relation, and have a closed-form formula in terms of binomial coefficients.
+
+## Definition
+C<sub>0</sub>, C<sub>1</sub>, C<sub>2</sub>, ..., C<sub>n</sub> are given by the formula   
+C<sub>n</sub> = $\frac{1}{n+1}$($\frac{2n}{n}$)
